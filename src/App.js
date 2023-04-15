b2512726ec566b34a01c2fa49c70cdaa7ebf23c6
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-04-15 18:03:24 +0200

    add advertisements page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import AdvertisementsPage from './components/advertisements/AdvertisementsPage.js';
+
 import './App.css';
 
 function App() {
-  return <div className="App">Hello React!</div>;
+  return <div className="App">
+    {/* aqui se muestra cada anuncio */}
+    <AdvertisementsPage/>
+  </div>;
 }
 
 export default App;
