76939f547abb5970a41512a6ab54227107548071
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-04-16 19:28:07 +0200

    add button with styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import AdvertisementsPage from './components/advertisements/AdvertisementsPage.js';
 
 import './App.css';
+import Button from './components/shared/Button';
 
 function App() {
   return <div className="App">
     {/* aqui se muestra cada anuncio */}
     <AdvertisementsPage/>
+    <Button variant="secondary" onClick={event => console.log(event)}>
+        Click me!
+      </Button>
+      <Button variant="primary" onClick={event => console.log(event)}>
+        Click me!
+      </Button>
+      <Button variant="primary" disabled onClick={event => console.log(event)}>
+        Click me!
+      </Button>
   </div>;
 }
 
