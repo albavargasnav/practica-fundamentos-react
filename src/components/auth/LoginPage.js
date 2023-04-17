746bc2c198ff6e944ea161276dfe3bc3a8860a2d
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-04-17 15:32:44 +0200

    add control inputs

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,28 +1,66 @@
+import { useState } from 'react';
 import Button from '../shared/Button';
 import { login } from './service';
 
 function LoginPage({ onLogin }) {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+    });
+
   const handleSubmit = async event => {
     event.preventDefault();
 
-    await login({
-      username: event.target.username.value,
-      password: event.target.password.value,
-    });
+    await login(credentials);
 
     // Estoy logueado
     onLogin();
   };
 
+  const handleChange = event => {
+    // if (event.target.name === 'username') {
+    //   setCredentials({ ...credentials, username: event.target.value });
+    // }
+    // if (event.target.name === 'password') {
+    //   setCredentials({ ...credentials, password: event.target.value });
+    // }
+
+    setCredentials({
+      ...credentials,
+      [event.target.value]: event.target.value,
+    });
+  };
+
+  const buttonDisabled = !credentials.username || !credentials.password;
+
   return (
     <div>
-      <h1>Log in to Adverts</h1>
+      <h1>Log into Adverts</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <Button type="submit" variant="primary">
+      <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          autoComplete={false}
+          value={credentials.username}
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          autoComplete={false}
+          value={credentials.password}
+        />
+        <Button type="submit" variant="primary" disabled={buttonDisabled}>
           Log in
         </Button>
+        <input
+          type="file"
+          name="photo"
+          onChange={event => {
+            console.log(event.target.files[0]);
+          }}
+        />
       </form>
     </div>
   );
