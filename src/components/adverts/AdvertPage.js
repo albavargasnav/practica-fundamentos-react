6090f090353e1a9cf6cfc4ad35a1fddb09665ad8
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-04-26 18:46:37 +0200

    Add advert id

--- src/components/adverts/AdvertPage.js
+++ src/components/adverts/AdvertPage.js
@@ -1,9 +1,11 @@
+import { useParams } from 'react-router-dom';
 import Layout from '../layout/Layout';
 
 const AdvertPage = props => {
+  const params = useParams();
   return (
     <Layout title="Advert detail" {...props}>
-      <div>Advert detail</div>
+      <div>Advert detail {params.advertId}</div>
     </Layout>
   );
 };
