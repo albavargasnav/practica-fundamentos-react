27cc56a2f4e9483af3a4354c454b976412a63950
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-04-26 18:29:08 +0200

    Add header navlink

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,7 @@ import logo, { ReactComponent as Icon } from '../../assets/pageicon.svg';
 import { logout } from '../auth/service';
 import classNames from 'classnames';
 import './Header.css';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = ({ className, isLogged, onLogout }) => {
   const handleLogoutClick = async () => {
@@ -13,11 +14,24 @@ const Header = ({ className, isLogged, onLogout }) => {
 
   return (
     <header className={classNames('header', className)}>
-    <div className="header-logo">
-        {/* <img src={logo} alt="iconode-react" /> */}
-        <Icon width="32" height="32" />
-      </div>
+     <Link to="/">
+        <div className="header-logo">
+          {/* <img src={logo} alt="advert-react" /> */}
+          <Icon width="32" height="32" />
+        </div>
+      </Link>
       <nav className="header-nav">
+      <NavLink
+          to="/adverts/new"
+          // className={({ isActive }) => (isActive ? 'selected' : '')}
+          // style={({ isActive }) => (isActive ? { color: 'red' } : null)}
+        >
+          New Advert
+        </NavLink>{' '}
+        |
+        <NavLink to="/adverts" end>
+          See latest adverts
+        </NavLink>
       {isLogged ? (
           <Button onClick={handleLogoutClick} className="header-button">
           Logout
