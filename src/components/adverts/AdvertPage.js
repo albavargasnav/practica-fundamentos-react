73e027d399f66313e8fa670ba5392fae9b0dde28
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-04-28 21:07:28 +0200

    Add AdvertPage data and placeholder image

--- src/components/adverts/AdvertPage.js
+++ src/components/adverts/AdvertPage.js
@@ -20,12 +20,23 @@ const AdvertPage = () => {
       });
   }, [params.Id, navigate]);
 
+  const advertSale = advert.sale ? 'En venta' : 'Se busca';
+
   // if (error?.status === 404) {
   //   return <Navigate to="/404" />;
   // }
   return (
-    <Layout title="Advert detail">
-      {advert && advert.id && <div>{advert.id}{advert.name}</div>}
+    <Layout title="Advert Page">
+      {advert && advert.id && 
+        <div>
+          <div className="advert-header">
+        <span className="advert-name">{advert.name}</span>
+      <div className="advert-username">Sale: {advert.tags}</div>
+      <div className="advert-username">Price: {advert.price}€</div>
+      <div className="advert-username">Tags: {advertSale}</div>
+          <span className="advert-separator">·</span>
+        </div>
+        </div>}
     </Layout>
   );
 };
